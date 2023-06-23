1209c8ceb05561741a4032ade77112220411ee70
Mangalam Kumar <mangalamkumar2002@gmail.com>
2023-06-23 22:34:48 +0530

    Working on the frontend part for both login and signup functionality

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,18 +1,17 @@
 const jwt = require('jsonwebtoken');
 const { hash, compare } = require("bcrypt");
-const { userModel } = require("../models/user.models");
+const { userModel } = require("../models/user.model");
 require('dotenv').config();
 
 const login = async (req, res)=>{
     try{
-        console.log('Reaching the Correct endpoint /login');
         const {email, password} = req.body;
         const isUserValid = await userModel.findOne({email});
         if(!isUserValid) return res.status(404).send({msg: "Wrong Credentials"});
         const result = await compare(password, isUserValid.password);
         if(result){
             const access_token = jwt.sign({userId: isUserValid._id}, process.env.JWT_SECRET_KEY, {expiresIn: '4h'});
-            return res.status(200).send({msg: 'Receiving from frontend', access_token, 'admin': isUserValid.name});
+            return res.status(200).send({msg: 'Receiving from frontend', access_token, 'user': isUserValid.name});
         }
         return res.status(404).send({msg: 'Wrong Credentials'});
     }catch(err){
@@ -23,11 +22,11 @@ const login = async (req, res)=>{
 
 const register = async (req, res)=>{
     try{
-        let {name, email, password, role} = req.body;
+        let {name, email, password} = req.body;
         let userExist = await userModel.findOne({email});
         if(userExist) return res.status(400).send({msg: 'User Already Exists'});
         password = await hash(password, Number(process.env.SALT_ROUNDS));
-        const newUser = new userModel({name, email, password, role});
+        const newUser = new userModel({name, email, password, role: 'user'});
         await newUser.save();
         res.status(200).send({msg: 'User Created'});
     }catch(err){
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const connection = require('./configs/db');
+const userRouter = require('./routes/user.routes');
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+
+app.use('/user', userRouter);
 
 app.listen(port, ()=>{
     connection();
